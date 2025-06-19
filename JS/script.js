0fe93ff2869504309ce2c9a3140648579265e980
samuel-yepes <syepesmolina@gmail.com>
2025-06-19 10:05:23 -0500

    validacion final de correo

--- JS/script.js
+++ JS/script.js
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     function validarEmail(email) {
-        const re = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+        const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         return re.test(String(email).toLowerCase());
     }
     });
